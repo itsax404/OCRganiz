87419203974708406a583c156b0a8a51faea1b33
itsax404 <quentin.davin@laposte.net>
2023-05-31 10:01:28 +0200

    Début de la reconnaissance du texte pour la classe 'Adresse' avec l'OCR

--- backend/images/image_processor.py
+++ backend/images/image_processor.py
@@ -4,6 +4,10 @@ import pytesseract
 
 class Image_Processor:
 
+	"""
+	TODO docstring car la classe est en construction
+	"""
+
 	def __init__(self, image, tesseract_dir: str):
 		self.image = image
 		self.tesseract_dir = tesseract_dir
@@ -12,10 +16,14 @@ class Image_Processor:
 		image = Image.open(self.image)
 		return image.crop(coordonnées)
 
-	def ocr_cropped_image(self, image: Image, lang: str = "fra") -> str:
+	def __ocr_cropped_image__(self, image: Image, lang: str = "fra") -> str:
 		pytesseract.pytesseract.tesseract_cmd = self.tesseract_dir
 		return pytesseract.image_to_string(image, lang=lang)
 
-	def crop_and_ocr(self, coordonnées: tuple[int, int, int, int], lang: str = "fra") -> str:
+	def __crop_and_ocr__(self, coordonnées: tuple[int, int, int, int], lang: str = "fra") -> str:
 		cropped_image = self.crop(coordonnées)
-		return self.ocr_cropped_image(cropped_image, lang=lang)
+		return self.__ocr_cropped_image__(cropped_image, lang=lang)
+
+	def detecte_adresse(self, coordonnées: tuple[int, int, int, int], lang: str = "fra") -> str:
+		texte_adresse = self.__crop_and_ocr__(coordonnées, lang=lang)
+		print(texte_adresse)
